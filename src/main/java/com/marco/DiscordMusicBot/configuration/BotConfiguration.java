373720be0238725e305920b27cf8bb1aa2fc2f69
Marco Lisandro Franco <104217387+Marc0Franc0@users.noreply.github.com>
2024-07-12 16:14:48 -0300

    update
    -Documentación en las clases
    -Clases de utilidades DiscordUtils y EmbedUtil
    -Comandos Play y Queue

--- src/main/java/com/marco/DiscordMusicBot/configuration/BotConfiguration.java
+++ src/main/java/com/marco/DiscordMusicBot/configuration/BotConfiguration.java
@@ -3,6 +3,8 @@ package com.marco.DiscordMusicBot.configuration;
 import com.marco.DiscordMusicBot.commands.ICommand;
 import com.marco.DiscordMusicBot.commands.CommandManager;
 import com.marco.DiscordMusicBot.commands.help.HelpCommand;
+import com.marco.DiscordMusicBot.commands.music.PlayCommand;
+import com.marco.DiscordMusicBot.commands.music.QueueCommand;
 import com.marco.DiscordMusicBot.service.CommandService;
 import lombok.extern.slf4j.Slf4j;
 import net.dv8tion.jda.api.JDA;
@@ -16,28 +18,37 @@ import java.util.List;
 
 @Configuration
 @Slf4j
-public class BotConfiguration {
+public class BotConfiguration{
     @Value("${bot.token}")
     private String botToken;
     private final CommandManager commandManager;
-
     @Autowired
     public BotConfiguration(CommandService commandService){
         List<ICommand> commandList = initializeCommands();
         this.commandManager = new CommandManager(commandList,commandService);
     }
+    /**
+     * Configures and initializes the JDA instance for the Discord bot.
+     *
+     * @return The configured JDA instance.
+     */
     @Bean
-    public JDA jda() {
-        //create
-        JDA jda = JDABuilder.createDefault(botToken).build();
-        //addEventListener
-        jda.addEventListener(commandManager);
-        return jda;
+    public JDA jda(){
+        return JDABuilder.createDefault(botToken)
+                .addEventListeners(commandManager)
+                .build();
     }
-    private List<ICommand>initializeCommands(){
+
+    /**
+     * Initializes and returns a list of bot commands.
+     *
+     * @return The list of initialized bot commands.
+     */
+    private List<ICommand> initializeCommands() {
         List<ICommand> commandList = new ArrayList<>();
-        //help
-        commandList.add(new HelpCommand());
+        commandList.add(new HelpCommand());   // Add HelpCommand to the command list
+        commandList.add(new PlayCommand());   // Add PlayCommand to the command list
+        commandList.add(new QueueCommand());  // Add QueueCommand to the command list
         return commandList;
     }
 }
